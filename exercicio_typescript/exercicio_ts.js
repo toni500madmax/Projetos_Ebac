6e0cdc42a84de0c6f45328a69ad15fc847403b02
toni500madmax <toni500madmax@gmail.com>
2024-03-19 18:15:53 -0300

    Exercício de Typescript e Compilador Typescript - Módulo 26 - Segunda Tentativa

---
+++ exercicio_typescript/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(x, y) {
+    return x * y;
+}
+const saudacao = (nome) => {
+    return `Olá ${nome}. Seja bem vindo(a).\n`;
+};
+console.log(multiplicacao(30, 45));
+console.log(saudacao("Katharine Colon"));
---
+++ exercicio_typescript/exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicacao(x: number, y: number): number {
+    return x * y;
+}
+
+const saudacao = (nome: string) => {
+    return `Olá ${nome}. Seja bem vindo(a).\n`;
+}
+
+console.log(multiplicacao(30, 45));
+console.log(saudacao("Katharine Colon"));
